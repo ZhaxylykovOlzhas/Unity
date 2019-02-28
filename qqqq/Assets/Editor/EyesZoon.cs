c9d3d9e9bb23e4954463b80b63480fcfc7899bd8
Олжас Жаксылыков <40593443+ZhaxylykovOlzhas@users.noreply.github.com>
2019-02-28 18:23:03 +0600

    Add files via upload

---
+++ qqqq/Assets/Editor/EyesZoon.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesZoon : MonoBehaviour
+{
+    [SerializeField]
+    private Enemy parent;
+
+private void OnTriggerEnter(Collider other)
+    {
+        parent.CheckSight();
+    }
+}
